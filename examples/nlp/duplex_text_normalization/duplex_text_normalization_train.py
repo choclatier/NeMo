c2f6032de1175aa4ba7dfe5aa85270afb837a674
Yang Zhang <yangzhang@nvidia.com>
2021-07-22 16:59:56 -0700

    inference nn tn with exported onnx
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- examples/nlp/duplex_text_normalization/duplex_text_normalization_train.py
+++ examples/nlp/duplex_text_normalization/duplex_text_normalization_train.py
@@ -93,32 +93,32 @@ def main(cfg: DictConfig) -> None:
     logging.info(f'Config Params: {OmegaConf.to_yaml(cfg)}')
 
     # Train the tagger
-    if cfg.tagger_model.do_training:
-        logging.info(
-            "================================================================================================"
-        )
-        logging.info('Starting training tagger...')
-        tagger_trainer, tagger_model = instantiate_model_and_trainer(cfg, TAGGER_MODEL, True)
-        exp_manager(tagger_trainer, cfg.get('tagger_exp_manager', None))
-        tagger_trainer.fit(tagger_model)
-        if cfg.tagger_model.nemo_path:
-            tagger_model.to(tagger_trainer.accelerator.root_device)
-            tagger_model.save_to(cfg.tagger_model.nemo_path)
-        logging.info('Training finished!')
-
-    # # Train the decoder
-    # if cfg.decoder_model.do_training:
+    # if cfg.tagger_model.do_training:
     #     logging.info(
     #         "================================================================================================"
     #     )
-    #     logging.info('Starting training decoder...')
-    #     decoder_trainer, decoder_model = instantiate_model_and_trainer(cfg, DECODER_MODEL, True)
-    #     exp_manager(decoder_trainer, cfg.get('decoder_exp_manager', None))
-    #     decoder_trainer.fit(decoder_model)
-    #     if cfg.decoder_model.nemo_path:
-    #         decoder_model.to(decoder_trainer.accelerator.root_device)
-    #         decoder_model.save_to(cfg.decoder_model.nemo_path)
+    #     logging.info('Starting training tagger...')
+    #     tagger_trainer, tagger_model = instantiate_model_and_trainer(cfg, TAGGER_MODEL, True)
+    #     exp_manager(tagger_trainer, cfg.get('tagger_exp_manager', None))
+    #     tagger_trainer.fit(tagger_model)
+    #     if cfg.tagger_model.nemo_path:
+    #         tagger_model.to(tagger_trainer.accelerator.root_device)
+    #         tagger_model.save_to(cfg.tagger_model.nemo_path)
     #     logging.info('Training finished!')
+
+    # # Train the decoder
+    if cfg.decoder_model.do_training:
+        logging.info(
+            "================================================================================================"
+        )
+        logging.info('Starting training decoder...')
+        decoder_trainer, decoder_model = instantiate_model_and_trainer(cfg, DECODER_MODEL, True)
+        exp_manager(decoder_trainer, cfg.get('decoder_exp_manager', None))
+        decoder_trainer.fit(decoder_model)
+        if cfg.decoder_model.nemo_path:
+            decoder_model.to(decoder_trainer.accelerator.root_device)
+            decoder_model.save_to(cfg.decoder_model.nemo_path)
+        logging.info('Training finished!')
 
 
 if __name__ == '__main__':
